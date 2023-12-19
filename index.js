388820ebece1c8431d3efb915664dd795a9e7681
PhilipSamuelsson <philip.samuelsson@iths.se>
2023-12-19 12:52:00 +0100

    * Added container for list item with delete button and checkbox.

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ list = document.getElementById('list')
 submitButton.addEventListener('click', (event) => {
     event.preventDefault()
     const text = textInput.value
-    list.innerHTML += `<li>${text}</li>`
+    list.innerHTML += `<div class="item-container"><input type="checkbox" class="checkbox"><p>${text}</p><button class="delete-button">Delete</button>
+    </div></li>`
     textInput.value = ''
     console.log(text)
 })
