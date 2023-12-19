d64a17b2b05920170312157468d3d9fc3ee98620
PhilipSamuelsson <philip.samuelsson@iths.se>
2023-12-19 16:36:24 +0100

    Changed a bug where checkboxes got reset after adding another item.

--- index.js
+++ index.js
@@ -1,37 +1,57 @@
 const submitButton = document.getElementById('submit-button')
 const textInput = document.getElementById('text-input')
 const list = document.getElementById('list')
-let deleteButtons = document.querySelectorAll('.delete-button')
-let checkboxes = document.querySelectorAll('.checkbox')
+const openModal = document.getElementById('open-modal')
+const modal = document.getElementById('modal')
+const closeModal = document.getElementsByClassName('close')
 
 submitButton.addEventListener('click', (event) => {
     event.preventDefault()
     const text = textInput.value
-    list.innerHTML += `<div class="item-container">
-    <input type="checkbox" class="checkbox">
-    <p>${text}</p>
-    <button class="delete-button">Delete</button>
-    </div>`
+
+    const itemContainer = document.createElement('div')
+    itemContainer.className = 'item-container mx-auto'
+
+    const checkbox = document.createElement('input')
+    checkbox.type = 'checkbox'
+    checkbox.className = 'checkbox'
+    checkbox.addEventListener('click', handleCheck)
+
+    const paragraph = document.createElement('p')
+    paragraph.textContent = text
+
+    const deleteButton = document.createElement('button')
+    deleteButton.className = 'delete-button'
+    deleteButton.textContent = 'Delete'
+    deleteButton.addEventListener('click', handleDelete)
+
+    itemContainer.appendChild(checkbox)
+    itemContainer.appendChild(paragraph)
+    itemContainer.appendChild(deleteButton)
+
+    list.appendChild(itemContainer)
+
     textInput.value = ''
     console.log(text)
-
-    deleteButtons = document.querySelectorAll('.delete-button')
-    checkboxes = document.querySelectorAll('.checkbox')
-
-    deleteButtons.forEach((button) => {
-        button.addEventListener('click', handleDelete)
-    })
-
-    checkboxes.forEach((checkbox) => {
-        checkbox.addEventListener('click', handleCheck)
-    })
 })
 
 function handleCheck(event) {
-    event.target.parentNode.classList.toggle('checked')
+    const checkbox = event.target
+    checkbox.parentNode.classList.toggle('checked')
     textInput.classList.toggle('checked')
 }
 
 function handleDelete(event) {
-    event.target.parentNode.remove()
+    const deleteButton = event.target
+    deleteButton.parentNode.remove()
 }
+
+openModal.addEventListener('click', () => {
+    modal.style.display = 'block'
+    openModal.style.display = 'none'
+})
+
+closeModal[0].addEventListener('click', () => {
+    modal.style.display = 'none'
+    openModal.style.display = 'block'
+})
